a5017e84101db190ddae2770efef664a5921dac3
athira <athira21041999@gmail.com>
2026-03-12 14:51:47 +0530

    rgreh

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,6 +118,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
